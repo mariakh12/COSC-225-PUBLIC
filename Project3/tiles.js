d93426eb97dcd65edac0e29a65dbad7b7492732b
mariakh12 <markhan26@amherst.edu>
2023-02-16 21:15:02 -0500

    Finalized project

--- Project3/tiles.js
+++ Project3/tiles.js
@@ -1,1 +1,25 @@
 
+function drawGrid()
+{   
+    let block = [];
+    let counter = 1 ;
+    const grid = document.querySelector(".grid") ;
+    for(let i = 1; i<= 10; i++)
+    {
+        for(let j=1 ; j<=10 ; j++)
+        {  
+            let changeR = counter*1.7 ;
+            let changeG= 255-(2.75*counter); 
+            let changeB = 150 + (counter*0.95) ;
+            let elt = document.createElement("div") ;
+            elt.style.backgroundColor = "rgb("+ changeR +" , "+ changeG +",  "+ changeB +")";
+            elt.style.width = "50px" ;
+            elt.style.height= "50px" ;
+            counter = counter +1 ; 
+            grid.appendChild(elt) ;
+             
+        }
+    }
+}
+
+
